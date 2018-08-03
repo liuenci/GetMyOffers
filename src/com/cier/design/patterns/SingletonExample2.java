7734749e9bfab1895d5639960bc7d1b760313db6
liuenci <154910381@qq.com>
2018-08-03 15:43:29 +0800

    设计模式之懒汉式单例模式

---
+++ src/com/cier/design/patterns/SingletonExample2.java
@@ -0,0 +1,25 @@
+package com.cier.design.patterns;
+
+/**
+ * 懒汉式
+ */
+public class SingletonExample2 {
+    // 声明变量
+    public static volatile SingletonExample2 instance = null;
+
+    // 私有化构造函数
+    private SingletonExample2(){
+
+    }
+    // 提供对外方法
+    public static SingletonExample2 getInstance(){
+        if (instance == null){
+            synchronized (SingletonExample2.class){
+                if (instance == null){
+                    instance = new SingletonExample2();
+                }
+            }
+        }
+        return instance;
+    }
+}
