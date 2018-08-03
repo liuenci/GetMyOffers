a69afe9ee5c52fce78b1e528b9772786f72c7644
liuenci <154910381@qq.com>
2018-08-03 15:39:18 +0800

    设计模式之饿汉式单例模式

--- src/com/cier/design/patterns/SingletonExample1.java
+++ src/com/cier/design/patterns/SingletonExample1.java
@@ -1,4 +1,18 @@
 package com.cier.design.patterns;
 
+/**
+ * 饿汉式
+ */
 public class SingletonExample1 {
+    // 直接创建对象
+    public static SingletonExample1 instance = new SingletonExample1();
+
+    // 私有化构造函数
+    private SingletonExample1(){
+
+    }
+    // 返回对象实例
+    public static SingletonExample1 getInstance(){
+        return instance;
+    }
 }
