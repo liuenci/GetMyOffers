9aabaa2d2cc52e1772995017b0d917171d859cef
liuenci <154910381@qq.com>
2018-08-03 21:42:09 +0800

    多个工厂方法

---
+++ src/com/cier/designPatterns/factory2/SendFactory.java
@@ -0,0 +1,24 @@
+package com.cier.designPatterns.factory2;
+
+import com.cier.designPatterns.factory1.MailSender;
+import com.cier.designPatterns.factory1.Sender;
+import com.cier.designPatterns.factory1.SmsSender;
+
+/**
+ * 多个工厂方法模式
+ */
+public class SendFactory {
+    public Sender produceMail(){
+        return new MailSender();
+    }
+
+    public Sender produceSms(){
+        return new SmsSender();
+    }
+
+    public static void main(String[] args) {
+        SendFactory factory = new SendFactory();
+        Sender sender = factory.produceMail();
+        sender.Send();
+    }
+}
