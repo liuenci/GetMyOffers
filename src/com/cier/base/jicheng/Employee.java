9057daa2897ac73a3d82c76145f95ec0630c1d7a
liuenci <154910381@qq.com>
2018-08-20 12:47:12 +0800

    ×Զ¯»¯Ìύ

--- src/com/cier/base/jicheng/Employee.java
+++ src/com/cier/base/jicheng/Employee.java
@@ -3,7 +3,6 @@ package com.cier.base.jicheng;
 public class Employee extends Person{
     String empID = "0000";
     public Employee(String id) {
-        super("12");
         empID = id;
     }
 }
