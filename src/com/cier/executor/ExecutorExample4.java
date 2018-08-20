17897ac9955a34ae840b5d65e2c9c6cdbdf23663
liuenci <154910381@qq.com>
2018-08-20 17:28:21 +0800

    auto update

---
+++ src/com/cier/executor/ExecutorExample4.java
@@ -0,0 +1,25 @@
+package com.cier.executor;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行。
+ */
+public class ExecutorExample4 {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++){
+            final int index = i;
+            executorService.execute(()->{
+                System.out.println(index);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}
