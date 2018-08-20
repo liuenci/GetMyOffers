cda43763927e29fca98395858cd6893cf503ad6d
liuenci <154910381@qq.com>
2018-08-20 11:42:40 +0800

    运算测试

---
+++ src/com/cier/base/yunsuan/Demo1.java
@@ -0,0 +1,9 @@
+package com.cier.base.yunsuan;
+
+public class Demo1 {
+    public static void main(String[] args) {
+
+        System.out.println(100%3);
+        System.out.println(100%3.0);
+    }
+}
