7288340f52a0de1c5330ef5ac71af43a50cf4977
liuenci <154910381@qq.com>
2018-08-20 17:13:51 +0800

    auto update

--- src/com/cier/executor/ExecutorExample2.java
+++ src/com/cier/executor/ExecutorExample2.java
@@ -8,6 +8,10 @@ import java.util.concurrent.Executors;
  * 创建一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待
  */
 public class ExecutorExample2 {
+    /**
+     * 因为线程池大小为3，每个任务输出index后sleep 2秒，所以每两秒打印3个数字。
+     * @param args
+     */
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         for (int i = 0; i < 10; i++){
