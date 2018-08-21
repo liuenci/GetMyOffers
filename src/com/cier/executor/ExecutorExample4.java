ee593fac2283c91a23f40ca7fa91342f1f3b8570
liuenci <154910381@qq.com>
2018-08-21 16:07:16 +0800

    auto update

--- src/com/cier/executor/ExecutorExample4.java
+++ src/com/cier/executor/ExecutorExample4.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 
 /**
  * 创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行。
+ * 结果依次输出，相当于顺序执行各个任务。
  */
 public class ExecutorExample4 {
     public static void main(String[] args) {
