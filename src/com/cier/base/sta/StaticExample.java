9249af884aed8d1e97e85fa09e7da7c2598f8be1
liuenci <154910381@qq.com>
2018-08-22 09:58:50 +0800

    auto update

---
+++ src/com/cier/base/sta/StaticExample.java
@@ -0,0 +1,13 @@
+package com.cier.base.sta;
+
+public class StaticExample {
+    final int a;
+
+    public StaticExample(int a) {
+        this.a = a;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
