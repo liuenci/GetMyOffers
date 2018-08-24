fc6ec90f48b6b3b88e43ca551f045d25918d31a8
liuenci <154910381@qq.com>
2018-08-24 19:45:06 +0800

    auto update

--- src/com/cier/gc/SlotGc.java
+++ src/com/cier/gc/SlotGc.java
@@ -3,6 +3,7 @@ package com.cier.gc;
 public class SlotGc {
     public static void main(String[] args) {
         byte[] holder = new byte[32 * 1024 * 1024];
+        // 进行垃圾回收
         System.gc();
     }
 }
