6cddd0b90e342bdae13481425010462139919cda
liuenci <154910381@qq.com>
2018-09-26 10:56:29 +0800

    测试基本数据类型是否相互影响

---
+++ src/com/cier/base/OIbject.java
@@ -0,0 +1,12 @@
+package com.cier.base;
+
+public class OIbject {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = num1;
+        System.out.println(num2);
+        num1 = 45;
+        System.out.println(num2);
+    }
+}
