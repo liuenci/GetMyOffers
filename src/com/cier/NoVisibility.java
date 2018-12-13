63c331fef9046e69f044a91749a1833e9da86a13
liuenci <154910381@qq.com>
2018-12-13 16:17:16 +0800

    非可见性

---
+++ src/com/cier/NoVisibility.java
@@ -0,0 +1,21 @@
+package com.cier;
+
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+    private static class ReaderThread extends Thread{
+        @Override
+        public void run() {
+            while (!ready);
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new ReaderThread().start();
+        Thread.sleep(1000);
+        number = 42;
+        ready = true;
+        Thread.sleep(10000);
+    }
+}
