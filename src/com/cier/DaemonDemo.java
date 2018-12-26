fd0e5511bbc131559a4b5af7786b908c407e5547
liuenci <154910381@qq.com>
2018-12-26 15:57:48 +0800

    守护线程

---
+++ src/com/cier/DaemonDemo.java
@@ -0,0 +1,24 @@
+package com.cier;
+
+public class DaemonDemo {
+    public static class DaemonT extends Thread{
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("I am alive");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new DaemonT();
+        t.setDaemon(true);
+        t.start();
+        Thread.sleep(2000);
+    }
+}
