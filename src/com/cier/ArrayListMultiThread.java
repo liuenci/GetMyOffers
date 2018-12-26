14dcefd2007a4cdb13f9c1607420f2cb120e4f6e
liuenci <154910381@qq.com>
2018-12-26 16:41:40 +0800

    ArrayList 线程不安全

---
+++ src/com/cier/ArrayListMultiThread.java
@@ -0,0 +1,25 @@
+package com.cier;
+
+import java.util.ArrayList;
+
+public class ArrayListMultiThread {
+    static ArrayList<Integer> a1 = new ArrayList<>(10);
+    public static class AddThread implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000000; i++ ){
+                a1.add(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new AddThread());
+        Thread t2 = new Thread(new AddThread());
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(a1.size());
+    }
+}
