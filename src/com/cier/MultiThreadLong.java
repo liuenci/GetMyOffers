b4344de38a772051a656815c75683674a3b33f87
liuenci <154910381@qq.com>
2018-12-26 15:41:19 +0800

    Long 类型原子性蓬安判定

---
+++ src/com/cier/MultiThreadLong.java
@@ -0,0 +1,38 @@
+package com.cier;
+// 原子性判定
+public class MultiThreadLong {
+    public static long t = 0;
+    public static class ChangeT implements Runnable{
+        private long to;
+        public ChangeT(long to){
+            this.to = to;
+        }
+        @Override
+        public void run() {
+            while (true){
+                MultiThreadLong.t = to;
+                Thread.yield();
+            }
+        }
+    }
+    public static class ReadT implements Runnable{
+        @Override
+        public void run() {
+            while (true){
+                long temp = MultiThreadLong.t;
+                if (temp != 111L && temp != -999L && temp != 333L && temp != -444L){
+                    System.out.println(temp);
+                }
+                Thread.yield();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new ChangeT(111L)).start();
+        new Thread(new ChangeT(-999L)).start();
+        new Thread(new ChangeT(333L)).start();
+        new Thread(new ChangeT(-444L)).start();
+        new Thread(new ReadT()).start();
+    }
+}
