411516a12490e111d5aeb748ed155c0320a3fdc6
liuenci <154910381@qq.com>
2019-02-11 22:25:42 +0800

    lock

---
+++ src/com/cier/ReenterLock.java
@@ -0,0 +1,31 @@
+package com.cier;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReenterLock implements Runnable{
+
+    public static ReentrantLock lock = new ReentrantLock();
+    public static int i = 0;
+    @Override
+    public void run() {
+        for (int j = 0; j < 10000000; j++) {
+            lock.lock();
+            try {
+                i++;
+            }finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReenterLock reenterLock = new ReenterLock();
+        Thread t1 = new Thread(reenterLock);
+        Thread t2 = new Thread(reenterLock);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(i);
+    }
+}
